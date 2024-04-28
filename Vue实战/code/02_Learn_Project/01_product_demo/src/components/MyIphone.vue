fefba2f8b24c4b19caa50840af4820241cb6ca0a
xzj <783638806@qq.com>
2024-04-28 09:25:52 +0800

    4.27

---
+++ Vue实战/code/02_Learn_Project/01_product_demo/src/components/MyIphone.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="product">
+    <h2>iphone15</h2>
+    <div>商品图片</div>
+    <div>商品价格: <span>¥{{price}}</span></div>
+    <p>iphone15, {{price}}}秒杀</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      price: 9999.9
+    }
+  }
+}
+</script>
+
