7209a8b4677213ece21e100dab0d531d3e7babdb
xzj <783638806@qq.com>
2024-05-09 23:45:14 +0800

    tabbar功能完善

--- Vue实战/code/hy-trip/src/components/tab-bar/tab-bar_01.vue
+++ Vue实战/code/hy-trip/src/components/tab-bar/tab-bar_01.vue
@@ -2,13 +2,25 @@
   // import tabbarData from '@/assets/data/tabbar.js'
   import tabbar from "@/assets/data/tabbar.js";
   import {getAssetURL} from "@/utils/load_assets.js";
+  import {ref} from "vue";
+  const curindex = ref(0);
+  import {useRouter} from "vue-router";
+  const router = useRouter();
+  const itemClick = (index, item) => {
+    curindex.value = index;
+    router.push(item.path);
+  }
 </script>
 
 <template>
   <div class="tab-bar">
     <template v-for="(item, index) in tabbar" :key="index">
-      <div class="tab-bar-item">
-        <img :src="getAssetURL(item.image)" alt="">
+      <div class="tab-bar-item"
+           :class="{active: curindex === index}"
+            @click="itemClick(index, item)"
+      >
+        <img v-if="curindex === index" :src="getAssetURL(item.imageActive)" alt="">
+        <img v-else :src="getAssetURL(item.image)" alt="">
         <span class="text">{{ item.text }}</span>
       </div>
     </template>
@@ -25,12 +37,16 @@
     flex: 1;
     border-top: 1px solid #ccc;
     height: 50px;
+
     .tab-bar-item {
       flex: 1;
       display: flex;
       justify-content: center;
       align-items: center;
       flex-direction: column;
+      &.active {
+        color: var(--primary-color);
+      }
       img {
         width: 36px;
       }
