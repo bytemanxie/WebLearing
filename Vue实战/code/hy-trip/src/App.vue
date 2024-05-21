ef90c2570459c490031755ca29673f61672cbbd1
xzj <783638806@qq.com>
2024-05-22 00:41:11 +0800

    拦截器loading

--- Vue实战/code/hy-trip/src/App.vue
+++ Vue实战/code/hy-trip/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import TabBar from './components/tab-bar/tab-bar_03.vue'
   import {useRoute} from "vue-router";
+  import Loading from "@/components/loading/loading.vue"
   const route = useRoute()
 </script>
 
@@ -9,6 +10,7 @@
     <router-view/>
     <tab-bar v-if="!route.meta.hideTabBar"/>
 <!--     <tab-bar/>-->
+    <loading/>
   </div>
 </template>
 
